306c4f034d0c0786912e23f1d79c40ecc2bd6f1d
Maggie <xungirl1024@gmail.com>
2024-11-15 17:50:08 +0800

    s

--- latex_recognition_project/test_api.py
+++ latex_recognition_project/test_api.py
@@ -1,11 +1,4 @@
-'''
-Author: Maggie xungirl1024@gmail.com
-Date: 2024-11-15 11:32:38
-LastEditors: Maggie xungirl1024@gmail.com
-LastEditTime: 2024-11-15 13:25:15
-FilePath: /latex_recognition_project/test_api.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-'''
+
 import requests
 import json
 
